b4b9fa0dd9fb936d46e76dda443627713512c8e3
DimVyach <112200474+DimVyach@users.noreply.github.com>
2022-11-01 23:45:53 +0100

    test_5

---
+++ script.js
@@ -0,0 +1,126 @@
+//namePerson = prompt("Kak тебя зовут?"); 
+//alert("Hello" + " " + namePerson  + "! " + "How are you?");
+ //console.log("how much");
+
+ //a = 5
+ //b = 7
+ //z = a + b
+ //console.log(z)
+
+ 
+ //nameUser = prompt("Whats your name?");
+ //alert("Hello" + " " + nameUser +" !How are you?" );
+
+ //confirm("Тебе есть 18?");
+
+ //date = new Date();
+ //year = date.getFullYear();
+ //console.log(date);
+
+ //yearOfBirth = +prompt("You year of birth?")
+ //userAge = year + yearOfBirth;
+ //console.log(year,yearOfBirth)
+ //console.log(userAge);
+
+ //userName = prompt("What is your name?");
+ //age = prompt("How old are you?");
+ //alert(`Hello ${userName}! Your age is ${age}. How are you?` );
+
+
+ //document.write("Hello World!");
+ //document.write("<h1>Hello world!</h1>");
+ //document.write(`<ul>
+   // <li>Title</li>
+    //<li>Title</li>
+    //<li>Title</li>
+    //<li>Title</li>
+    //<li>Title</li>
+  //<ul>`);
+
+  //userName = prompt("What is your name?");
+  //age = prompt("How old are you?");
+  //document.write(`
+  //<h1 class= red>CV</h1>
+  //<h3>My name is ${userName} </h3>
+  //<h5>I m ${age} years old! </h5>
+//`);
+
+
+//userName = prompt("What is your name?").trim();
+//console.log(`My name is ${userName}!`);
+
+//animal = "Elephant";
+//console.log(animal.length);
+//console.log(animal[3]);
+//console.log(animal[animal.length-1]);//t
+
+
+    //indexOf();
+//color = "Yellow"
+//console.log(color.indexOf("e"));//1
+//console.log(color.indexOf("p"));//-1
+//console.log(color.indexOf("o"));//4
+
+     //NUMBER METHODS
+     //Math.floor  (уменьшает до меньшего значения)
+     //Math.ceil   (увеличивает до большего значения)
+     //Math.round  (округляет математическим способом)
+     //Math.trunc  (убирает все значения после точки)
+
+
+//value = 12.48098868589
+//console.log(Math.floor(value));//12
+//console.log(Math.ceil(value));//13
+//console.log(Math.round(value));//12
+//console.log(Math.trunc(value));//12
+
+           //% остаток от деления  (чтобы узнать остаток от деления)
+  //x = 10%3
+  //console.log(x);          
+           
+        //isNAN(value);(проверяет является ли значение числом)
+//isNaN(10); //false
+//isNaN("10"); //false
+//isNAN("ddddfg"); //true
+
+          //typeof (строгая проверка типа)
+  //typeof 10  //number
+  //typeof "10"  //string
+ //typeof "Hello"  //string
+ //typeof null  //object
+ //typeof undefined //undefined
+
+
+              //null - является обьектом
+     //hello = null
+     //hello = prompt("Anithing?");
+     //alert(hello);        
+
+            //undefined
+ //let hello
+ //console.log(hello);//undefined
+ 
+// number1 = +prompt("Write the first number");
+ //number2 = +prompt("Write the second number");
+ //alert(`User entered ${number1} and ${number2}`);
+  //result1 = (number1 + number2);
+  //result2 = (number1 - number2);
+  //result3 = (number1 * number2);
+  //result4 = (number1 / number2);
+  //alert(result1);
+ 
+
+//a = +prompt("Write the first number");
+//b = +prompt("Write the second number"); 
+  //alert(`${a} + ${b} = ${a + b}`);
+  //alert(`${a} - ${b} = ${a - b}`);
+  //alert(`${a} * ${b} = ${a * b}`);
+  //alert(`${a} / ${b} = ${a / b}`);
+
+time = +prompt("What time is it?");
+seconds = time * 3600;
+alert(`It\`s ${seconds} seconds`); 
+
+
+
+
